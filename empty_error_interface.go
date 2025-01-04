1083a7f9f52073e0953d22851b11d09d4a760d06
Abu Sayed <sayedemon53@gmail.com>
2025-01-04 21:34:28 +0600

    TAG 6: Empty error interface

---
+++ empty_error_interface.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero")
+	}
+	return a / b, nil
+}
+
+func main() {
+	res, err := divide(4, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(res)
+
+}
